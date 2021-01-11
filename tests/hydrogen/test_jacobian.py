66762474f043f9131cce812a383c1285fd5b4874
Riccardo Malpica Galassi <riccardo.malpicagalassi@uniroma1.it>
2021-01-11 16:40:45 +0100

    bug fix on evals plot

--- tests/hydrogen/test_jacobian.py
+++ tests/hydrogen/test_jacobian.py
@@ -80,8 +80,8 @@ plt.tight_layout()
 plt.show()
 
 #plot eigenvalues
-logevals = np.clip(np.log10(np.abs(evals)),0,100)*np.sign(evals.real)
-logevals_an = np.clip(np.log10(np.abs(evals_an)),0,100)*np.sign(evals_an.real)
+logevals = np.clip(np.log10(np.abs(evals.real)),0,100)*np.sign(evals.real)
+logevals_an = np.clip(np.log10(np.abs(evals_an.real)),0,100)*np.sign(evals_an.real)
 print('plotting eigenvalues...')
 fig, ax = plt.subplots(figsize=(6,4))
 for idx in range(evals.shape[1]):
