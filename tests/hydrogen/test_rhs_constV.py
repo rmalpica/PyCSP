6072dcd2f34662b2b632c2def94e9138e6da580b
Riccardo Malpica Galassi <riccardo.malpicagalassi@uniroma1.it>
2021-01-30 12:33:44 +0100

    adding constant volume

--- tests/hydrogen/test_rhs_constV.py
+++ tests/hydrogen/test_rhs_constV.py
@@ -11,19 +11,23 @@ import src.cspFunctions as csp
 
 #create gas from original mechanism file hydrogen.cti
 gas = csp.CanteraCSP('hydrogen.cti')
-gas.set_problemtype('const_p')
+
 #set the gas state
 T = 1000
 P = ct.one_atm
 #gas.TPX = T, P, "H2:2.0, O2:1, N2:3.76"
 gas.TP = T, P
+
 gas.set_equivalence_ratio(1.0, 'H2', 'O2:1, N2:3.76')
+
+rho = gas.density
+gas.set_problemtype('const_v',rho)
 
 #equilibrium
 #gas.equilibrate('HP')
 
 #integrate ODE
-r = ct.IdealGasConstPressureReactor(gas)
+r = ct.IdealGasReactor(gas)
 sim = ct.ReactorNet([r])
 time = 0.0
 states = ct.SolutionArray(gas, extra=['t'])
@@ -32,15 +36,15 @@ RHS = []
 splitRHS = []
 varnames = np.array(gas.species_names+['Temperature'])
 sim.set_initial_time(0.0)
-while sim.time < 1.5e-3:
+while sim.time < 1.e-3:
     sim.step()
     states.append(r.thermo.state, t=sim.time)
-    print('%10.3e %10.3f %10.3f %14.6e' % (sim.time, r.T, r.thermo.P, r.thermo.u))
-    rhs = gas.rhs_const_p()
-    Smat = gas.generalized_Stoich_matrix()
+    print('%10.3e %10.3f %10.3f %10.3f %14.6e' % (sim.time, r.T, r.thermo.P, r.thermo.density, r.thermo.u))
+    rhs = gas.rhs_const_v()
+    Smat = gas.generalized_Stoich_matrix_const_v()
     rvec = gas.R_vector()
     splitrhs = np.dot(Smat,rvec)
-    checksplitrhs = np.isclose(rhs, splitrhs, rtol=1e-6, atol=0, equal_nan=False)
+    checksplitrhs = np.isclose(rhs, splitrhs, rtol=1e-6, atol=1e-6, equal_nan=False)
     if(np.any(checksplitrhs == False)):
         idx = np.array([*range(len(rhs))]) 
         print('Mismatch between numerical RHS and S.r')
@@ -59,22 +63,22 @@ plt.subplot(2, 2, 1)
 plt.plot(states.t, states.T)
 plt.xlabel('Time (s)')
 plt.ylabel('Temperature (K)')
-plt.xlim(0., 0.002)
+plt.xlim(0., 0.001)
 plt.subplot(2, 2, 2)
 plt.plot(states.t, states.X[:,gas.species_index('OH')])
 plt.xlabel('Time (s)')
 plt.ylabel('OH Mole Fraction')
-plt.xlim(0., 0.002)
+plt.xlim(0., 0.001)
 plt.subplot(2, 2, 3)
 plt.plot(states.t, states.X[:,gas.species_index('H')])
 plt.xlabel('Time (s)')
 plt.ylabel('H Mole Fraction')
-plt.xlim(0., 0.002)
+plt.xlim(0., 0.001)
 plt.subplot(2, 2, 4)
 plt.plot(states.t, states.X[:,gas.species_index('H2')])
 plt.xlabel('Time (s)')
 plt.ylabel('H2 Mole Fraction')
-plt.xlim(0., 0.002)
+plt.xlim(0., 0.001)
 plt.tight_layout()
 plt.show()
 
