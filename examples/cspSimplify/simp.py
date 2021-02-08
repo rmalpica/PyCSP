8b2d8715ae800301bccdaec6f5f30eb60670870a
Riccardo Malpica Galassi <riccardo.malpicagalassi@uniroma1.it>
2021-02-08 20:39:35 +0100

    'wip'

--- examples/cspSimplify/simp.py
+++ examples/cspSimplify/simp.py
@@ -6,7 +6,6 @@ import cantera as ct
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.colors as clr
-import sys
 import PyCSP.Functions as csp
 import PyCSP.Simplify as simp
 
@@ -18,27 +17,28 @@ dtl_mech = csp.CanteraCSP('gri30.cti')
 #set the gas state
 T = 1000
 P = ct.one_atm
-#gas.TPX = T, P, "H2:2.0, O2:1, N2:3.76"
 dtl_mech.TP = T, P
+dtl_mech.constP = P
 dtl_mech.set_equivalence_ratio(1.0, 'CH4', 'O2:1, N2:3.76')
 
 
-#integrate ODE and dump test data
+#integrate ODE with detailed mech
 r = ct.IdealGasConstPressureReactor(dtl_mech)
 sim = ct.ReactorNet([r])
 time = 0.0
-states = ct.SolutionArray(dtl_mech, extra=['t'])
+states = ct.SolutionArray(dtl_mech, 1, extra={'t': [0.0], 'rhsT': [0.0]})
 
 sim.set_initial_time(0.0)
 while sim.time < 1000:
     sim.step()
-    states.append(r.thermo.state, t=sim.time)
+    states.append(r.thermo.state, t=sim.time, rhsT=dtl_mech.source[-1])
     #print('%10.3e %10.3f %10.3f %14.6e' % (sim.time, r.T, r.thermo.P, r.thermo.u))
 
 dataset = np.concatenate((states.Y,states.T[:,np.newaxis],states.P[:,np.newaxis]),axis=1)
 print('Dataset created, start processing...')
 
-dataset = dataset[::16]
+#coarsen dataset (to speed up)
+dataset = dataset[::64]
 
 #init simplifier
 simplifier = simp.CSPsimplify(dtl_mech,dataset)
@@ -47,6 +47,8 @@ simplifier = simp.CSPsimplify(dtl_mech,dataset)
 simplifier.targetset = {'CH4','O2','N2','HCO','H2O','CO2'}
 simplifier.problemtype = 'constP'
 simplifier.scaled = True
+simplifier.csprtol = 1.0e-2
+simplifier.cspatol = 1.0e-8
 
 simplifier.dataset_info()
 
@@ -57,13 +59,14 @@ print('Done processing')
 
 print('Start pruning...may take a while')
 #loop over thresholds
-thr = np.arange(0.01, 0.99, 0.01)
+thr = np.arange(0.01, 1, 0.01)
+
 simp_mech = []
 prev_species = dtl_mech.species_names
 for i in range(len(thr)):
     species, reactions = simplifier.simplify_mechanism(thr[i])
-    simp = ct.Solution(thermo='IdealGas', kinetics='GasKinetics', species=species, reactions=reactions)
-    if simp.species_names != prev_species: 
+    simp = csp.CanteraCSP(thermo='IdealGas', kinetics='GasKinetics', species=species, reactions=reactions)
+    if simp.species_names != prev_species:  #append only if different from previous one
         simp_mech.append(simp)
         prev_species = simp.species_names
 
@@ -74,29 +77,39 @@ all_states_simp = []
 for i in range(nmech):
     # Re-run the ignition problem with the simplified mechanisms
     simp_mech[i].TP = T,P
+    simp_mech[i].constP = P
     simp_mech[i].set_equivalence_ratio(1.0, 'CH4', 'O2:1, N2:3.76')
     r = ct.IdealGasConstPressureReactor(simp_mech[i])
     sim = ct.ReactorNet([r])
-    states_simp = ct.SolutionArray(simp_mech[i], extra=['t'])
+    states_simp = ct.SolutionArray(simp_mech[i], 1, extra={'t': [0.0], 'rhsT': [0.0]})
     
     sim.set_initial_time(0.0)
     while sim.time < 1000:
         sim.step()
-        states_simp.append(r.thermo.state, t=sim.time)
+        states_simp.append(r.thermo.state, t=sim.time, rhsT=simp_mech[i].source[-1])
     all_states_simp.append(states_simp)
         
 
 
 C = [np.random.choice(list(clr.CSS4_COLORS.values())) for i in range(nmech)] 
-plt.plot(states.t, states.T, lw=2, color='b' ,
-         label='Detailed')
+fig, ax = plt.subplots(figsize=(6,4))
+ax.plot(states.t, states.T, lw=2, color='b', label='Detailed')
 for i in range(nmech):
-    plt.plot(all_states_simp[i].t, all_states_simp[i].T, lw=2, color = C[i],
+    ax.plot(all_states_simp[i].t, all_states_simp[i].T, lw=2, color = C[i],
          label='M#{0}, Ns={1}, Nr={2}'.format(i, simp_mech[i].n_species, simp_mech[i].n_reactions))
-plt.xlabel('Time (s)')
-plt.ylabel('Temperature (K)')
-plt.legend(loc='lower right')
-plt.xlim(0, 2)
-plt.tight_layout()
+ax.set_xlabel('Time (s)')
+ax.set_ylabel('Temperature (K)')
+ax.legend(loc='lower right')
+ax.set_xlim([0, 2])
+plt.show()
+
+fig, ax = plt.subplots(figsize=(6,4))
+ax.axhline(states.t[states.rhsT.argmax()], color='b')
+for i in range(nmech):
+    delay = all_states_simp[i].t[all_states_simp[i].rhsT.argmax()]
+    ax.scatter(simp_mech[i].n_species, delay, color='red')
+ax.set_xlabel('# of species')
+ax.set_ylabel('ignition delay time [s]')
+plt.show
 
 
