adc525c7143038e831d1ab89bee4e11e7e0e4887
Riccardo Malpica Galassi <riccardo.malpica@gmail.com>
2021-11-19 15:25:21 +0100

    adding ct2cti

--- examples/cspSimplify/simp_TSR.py
+++ examples/cspSimplify/simp_TSR.py
@@ -2,6 +2,7 @@
 """
 @author: Riccardo Malpica Galassi, Sapienza University, Roma, Italy
 """
+import os
 import cantera as ct
 import numpy as np
 import matplotlib.pyplot as plt
@@ -9,7 +10,7 @@ import matplotlib.colors as clr
 import PyCSP.Functions as csp
 import PyCSP.Simplify as simp
 import PyCSP.utils as utils
-from PyCSP.soln2ck import write
+from PyCSP.soln2cti import write
 
 
 FuComp = 'CH4'
@@ -73,7 +74,8 @@ for i in range(len(thr)):
     simp = csp.CanteraCSP(thermo='IdealGas', kinetics='GasKinetics', species=species, reactions=reactions)
     if sorted(simp.species_names) != prev_species:  #append only if different from previous one
         simp_mech.append(simp)
-        write(simp, output_filename='skeletal_N'+str(simp.n_species)+'_R'+str(simp.n_reactions)+'.inp', skip_thermo=True, skip_transport=True)
+        #write(simp, output_filename='skeletal_N'+str(simp.n_species)+'_R'+str(simp.n_reactions)+'.inp', skip_thermo=True, skip_transport=True)
+        write(simp, output_filename='skeletal_N'+str(simp.n_species)+'_R'+str(simp.n_reactions)+'.inp')
         prev_species = sorted(simp.species_names)
 
 nmech = len(simp_mech)
@@ -81,6 +83,16 @@ print('%i mechanisms found' % (nmech))
 
 
 print('Assessing skeletal mechanisms performance over a range of conditions...')
+
+#import DTL
+dtl_mech = csp.CanteraCSP('gri30.cti')
+
+#import Skeletal(s)
+read_mech = sorted([f for f in os.listdir('./') if f.startswith('skeletal')])
+simp_mech = []
+for i in range(len(read_mech)):
+    simp_mech.append(csp.CanteraCSP(read_mech[i]))
+print('%i mechanisms found' % len(simp_mech))
 
 T_list = np.arange(1000,1800,100)
 phi_list = [0.5, 0.7, 1.0, 1.2, 1.5]
@@ -90,30 +102,34 @@ nPhi = len(phi_list)
 
 all_states_dtl = []
 all_idt_dtl = []
-
+all_equi_dtl = []
 for eqratio in phi_list:
     for temperature in T_list:
             # Re-run the ignition problem with the detailed mechanisms
             states = utils.integrate_batch_constP(dtl_mech,temperature,P,eqratio,FuComp,OxComp,1000)            
             idt = states.t[states.rhsT.argmax()]
+            equi = states.T[-1]
             all_states_dtl.append(states)
             all_idt_dtl.append(idt)
+            all_equi_dtl.append(equi)
 
   
 all_states_simp = []
 all_idt_simp = []
+all_equi_simp = []
 for i in range(nmech):   
     for eqratio in phi_list:    
         for temperature in T_list:
             # Re-run the ignition problem with the simplified mechanisms
             states = utils.integrate_batch_constP(simp_mech[i],temperature,P,eqratio,FuComp,OxComp,1000)
             idt = states.t[states.rhsT.argmax()]
+            equi = states.T[-1]
             all_states_simp.append(states)
             all_idt_simp.append(idt)
+            all_equi_simp.append(equi)
         
 
-#compute ignition delays
-
+#compute ignition delay errors
 all_idt_err = []
 for i in range(nmech):
     for j in range(nPhi):
@@ -124,7 +140,17 @@ for i in range(nmech):
 all_idt_err = np.array(all_idt_err).reshape(nmech,nPhi,nTemp)
 
 
+#compute equilibrium errors
+all_equi_err = []
+for i in range(nmech):
+    for j in range(nPhi):
+        for k in range(nTemp):            
+            err_equi = abs((all_equi_simp[i*nPhi*nTemp +j*nTemp +k]-all_equi_dtl[j*nTemp+k])/all_equi_dtl[j*nTemp+k])*100
+            all_equi_err.append(err_equi)
 
+all_equi_err = np.array(all_equi_err).reshape(nmech,nPhi,nTemp)
+
+plt.ioff()
 for i in range(nmech):
     fig, ax = plt.subplots(figsize=(6,4))
     for j in range(nPhi):
@@ -138,8 +164,22 @@ for i in range(nmech):
     ax.set_ylim([1e-3, 1e2])
     ax.legend()
     plt.savefig('ign_delay_error_sk'+str(simp_mech[i].n_species)+'_'+str(simp_mech[i].n_reactions)+'.png', dpi=800, transparent=False)
-
+    
         
+for i in range(nmech):
+    fig, ax = plt.subplots(figsize=(6,4))
+    for j in range(nPhi):
+        label='phi = '+str(phi_list[j])
+        ax.plot(1000/T_list, all_equi_err[i,j], marker='.', label=label)
+    title='# of species: '+str(simp_mech[i].n_species)
+    ax.set_title(title)
+    ax.set_xlabel('1000/T [1/K]')
+    ax.set_ylabel('Equilibrium Temp relative Error [%]')
+    ax.set_yscale('log')
+    ax.set_ylim([1e-6, 1e2])
+    ax.legend()
+    plt.savefig('equilibrium_error_sk'+str(simp_mech[i].n_species)+'_'+str(simp_mech[i].n_reactions)+'.png', dpi=800, transparent=False)
+    
 
 
 # =============================================================================
